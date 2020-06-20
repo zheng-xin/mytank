da9d46eaebd659c4ae779ee4972241ec8ee6b8bb
zheng-xin <1690092000@qq.com>
2020-06-20 19:51:21 +0800

    create a new branch tank

--- src/main/java/com/zhengxin/tank/clent/Client.java
+++ src/main/java/com/zhengxin/tank/clent/Client.java
@@ -1,5 +1,15 @@
 package com.zhengxin.tank.clent;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoop;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 /**
  * @Auther: zhengxin
  * @Date: 2020/6/20 - 06 - 20 - 15:53
@@ -8,7 +18,20 @@ package com.zhengxin.tank.clent;
  */
 public class Client {
     private Client(){};
+    public static Client INSTANCE = new Client();
     public void conect(){
-        
+        EventLoopGroup group = new NioEventLoopGroup(1);
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+            ChannelFuture f = bootstrap.group(group).
+                    channel(NioSocketChannel.class).
+                    handler(new ClientChannelInitializer()).
+                    bind("localhost", 8888).
+                    sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
