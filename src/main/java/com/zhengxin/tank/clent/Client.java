e9900f1926cc0f59adf591da2db448e118c30f4d
zheng-xin <1690092000@qq.com>
2020-06-20 16:08:39 +0800

    create a new branch tank

---
+++ src/main/java/com/zhengxin/tank/clent/Client.java
@@ -0,0 +1,14 @@
+package com.zhengxin.tank.clent;
+
+/**
+ * @Auther: zhengxin
+ * @Date: 2020/6/20 - 06 - 20 - 15:53
+ * @Description: com.zhengxin.tank.clent
+ * @version: 1.0
+ */
+public class Client {
+    private Client(){};
+    public void conect(){
+        
+    }
+}
